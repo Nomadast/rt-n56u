f4540c631ac0a303d94bc08b6bf4539283ddaaac
Nomadast <ashokimov@mail.ru>
2026-01-06 00:18:18 +0500

    Update board.h

--- trunk/configs/boards/RT-N14U/board.h
+++ trunk/configs/boards/RT-N14U/board.h
@@ -8,9 +8,9 @@
 #define BOARD_MODEL_URL		"http://www.asus.com/Networking/RTN14U/"
 #define BOARD_CHIP_ID		"MT7620"
 
-/* GPIO */
-#define BOARD_GPIO_LED_INET	    0  /* Исправлено вами */
-#define BOARD_GPIO_LED_POWER	4  /* Исправлено вами */
+/* GPIO (индикаторы) */
+#define BOARD_GPIO_LED_INET	    0
+#define BOARD_GPIO_LED_POWER	4
 #define BOARD_GPIO_LED_LAN	    1
 #define BOARD_GPIO_LED_WIFI	    72
 #define BOARD_GPIO_LED_USB	    71
@@ -18,7 +18,7 @@
 #define BOARD_GPIO_BTN_RESET	1
 #define BOARD_GPIO_BTN_WPS	    2
 
-/* Эфирные параметры (исправляют вашу ошибку) */
+/* Настройки Ethernet-порта (Исправляют отсутствие пинга) */
 #define BOARD_HAS_5G_11AC	    0
 #define BOARD_NUM_ANT		    2
 #define BOARD_NUM_ANT_2G_TX	    2
@@ -26,9 +26,10 @@
 #define BOARD_NUM_ANT_5G_TX	    0
 #define BOARD_NUM_ANT_5G_RX	    0
 
-/* Параметры портов (исправляют ошибку EPHY_1000) */
-#define BOARD_HAS_EPHY_L1000    0
-#define BOARD_HAS_EPHY_W1000    0
 #define BOARD_NUM_ETH_LEDS	    0
 #define BOARD_HAS_EPHY_LNK_LED	1
 #define BOARD_HAS_EPHY_WPD	    1
+
+/* Это заставит сетевой чип работать на 100Мбит (как у N14U) */
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
